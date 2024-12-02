836fae16d22b80dbc9510cf99aa09393683bfd39
BILL <tatemsabill@gmail.com>
2024-12-02 13:02:05 +0100

    Initialization

---
+++ src/main/java/ML/life/controller/TestController.java
@@ -0,0 +1,14 @@
+package ML.life.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "")
+public class TestController {
+	@GetMapping(path = "")
+	public String getString(){
+		return "Test de chaine";
+	}
+}
---
+++ src/test/java/ML/life/MarieLouiseApplicationTests.java
@@ -0,0 +1,13 @@
+package ML.life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarieLouiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
